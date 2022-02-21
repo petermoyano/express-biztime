36aabe45010748d293ae025c974a04c5f5485788
Pedro Moyano <pedromoyano454@gmail.com>
2022-02-21 18:01:32 -0300

    database conection

--- app.js
+++ app.js
@@ -9,22 +9,31 @@ const ExpressError = require("./expressError")
 app.use(express.json());
 
 
+
+
+
+
+
+
+
+
+
 /** 404 handler */
 
 app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    return res.json({
+        error: err,
+        message: err.message
+    });
 });
 
 
